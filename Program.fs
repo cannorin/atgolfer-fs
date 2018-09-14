69175383216e543d87954866e7d5dc803725c40e
cannorin <cannorin@users.noreply.github.com>
2018-09-15 03:15:53 +0900

    Get rid of AtCoder

--- Program.fs
+++ Program.fs
@@ -1,5 +1,6 @@
-﻿open System
+open System
 open System.IO
+open System.Text
 open FSharp.Control
 open FSharp.Data
 open FSharp.CommandLine.Commands
@@ -9,11 +10,11 @@ open CoreTweet
 
 [<Literal>]
 let ContestsUrl = "https://kenkoooo.com/atcoder/atcoder-api/info/contests"
-type Contests = JsonProvider<ContestsUrl>
+type Contests = JsonProvider<ContestsUrl, Encoding="UTF-8">
 
 [<Literal>]
 let MergedProblemsUrl = "https://kenkoooo.com/atcoder/atcoder-api/info/merged-problems"
-type MergedProblems = JsonProvider<MergedProblemsUrl>
+type MergedProblems = JsonProvider<MergedProblemsUrl, Encoding="UTF-8">
 
 let inline getContests () = Contests.AsyncLoad ContestsUrl
 
@@ -22,66 +23,6 @@ let inline getMergedProblems () =
     let! data = MergedProblems.AsyncLoad MergedProblemsUrl
     let! raw  = JsonValue.AsyncLoad MergedProblemsUrl
     return data, raw.ToString(JsonSaveOptions.None)
-  }
-
-type TestCase = {
-  caseName: string
-  status:   string
-  execTime: TimeSpan
-  memory:   int
-}
-
-type Submission = {
-  url: string
-  id: string
-  sourceCode: string
-  info: Map<string, string>
-  testCases: TestCase list
-}
-
-let getSubmission url =
-  async {
-    let! page = HtmlDocument.AsyncLoad url
-
-    let id =
-      page.Descendants ["span"]
-      |> Seq.filter (fun x -> x.HasClass("h2"))
-      |> Seq.map    (fun x -> x.InnerText())
-      |> Seq.find   (String.startsWith "Submission #")
-      |> String.splitBy '#' |> Seq.item 1
-
-    let src =
-      page.Descendants ["pre"]
-      |> Seq.find (fun x -> x.HasId "submission-code")
-      |> HtmlNodeExtensions.InnerText
-
-    let sub_info :: tc_summary :: tc_data :: _ = page.Descendants ["table"] |> List.ofSeq
-    
-    let info =
-      seq {
-        for tr in sub_info.Descendants ["tr"] do
-          let th = tr.Descendants["th"] |> Seq.head
-          let td = tr.Descendants["td"] |> Seq.head
-          yield 
-            th.InnerText() |> String.replace ' ' '_' |> String.toLower None,
-            td.InnerText() |> String.trim
-      } |> Map.ofSeq
-    
-    let tcases =
-      [
-        for tr in (tc_data.Descendants["tbody"] |> Seq.head).Descendants ["tr"] do
-          let td = tr.Descendants["td"] |> Array.ofSeq
-          yield {
-            caseName = td.[0].InnerText()
-            status   = td.[1].InnerText()
-            execTime = td.[2].InnerText() |> String.splitBy " ms"
-                       |> Seq.head |> float |> TimeSpan.FromMilliseconds
-            memory   = td.[3].InnerText() |> String.splitBy " KB"
-                       |> Seq.head |> int
-          }
-      ]
-
-    return { url = url; id = id; sourceCode = src; info = info; testCases = tcases }
   }
 
 let inline strarg name = 
@@ -147,34 +88,24 @@ let mainCmd =
           let url = sprintf "https://beta.atcoder.jp/contests/%s/submissions/%i"
                             shortestContestId
                             shortestSubmissionId
-          let! newSubmission = getSubmission url
-
           let contestTitle = (contestFromId shortestContestId).Title
           let problemTitle = problem.Title
-          let newUser      = problem.ShortestUserId.JsonValue.ToString()
-          let newSize      = newSubmission.info |> Map.find "code_size"
+          let newUser      = problem.ShortestUserId.JsonValue.InnerText()
+          let newSize      = problem.SourceCodeLength.Value
           
-          let! textBody =
+          let textBody =
             if lastSubmissionId.IsSome then
-              async {
-                let! oldSubmission =
-                  getSubmission <|
-                    sprintf "https://beta.atcoder.jp/contests/%s/submissions/%i"
-                            lastProblem.Value.ShortestContestId.Value
-                            lastSubmissionId.Value
-                let oldUser = lastProblem.Value.ShortestUserId.JsonValue.ToString()
-                let oldSize = oldSubmission.info |> Map.find "code_size"
-                if newUser = oldUser then
-                  return sprintf "%s さんが自身のショートコードを更新しました！ (%s → %s)" 
-                                 newUser                               oldSize newSize
-                else
-                  return sprintf "%s さんが %s さんからショートコードを奪取しました！ (%s → %s)"
-                                 newUser oldUser                            oldSize newSize
-              }
+              let oldUser = lastProblem.Value.ShortestUserId.JsonValue.InnerText()
+              let oldSize = lastProblem.Value.SourceCodeLength.Value
+              if newUser = oldUser then
+                sprintf "%s さんが自身のショートコードを更新しました！ (%i → %i)" 
+                        newUser                               oldSize newSize
+              else
+                sprintf "%s さんが %s さんからショートコードを奪取しました！ (%i → %i)"
+                        newUser oldUser                            oldSize newSize
             else
-              Async.returnValue <|
-                sprintf "%s さんがショートコードを打ち立てました！ (%s)"
-                        newUser                            newSize
+              sprintf "%s さんがショートコードを打ち立てました！ (%i)"
+                      newUser                            newSize
           let text =
             sprintf "%s: %s\n%s\n%s" contestTitle problemTitle textBody url
           eprintfn "%s" text
